22f9493c48b628c7120482a57dfd5ae858d5d54b
Yuriy Veres <yveres@tmg.io>
2022-12-10 09:36:48 +0200

    Fix ping fail state

--- utils.js
+++ utils.js
@@ -1,8 +1,8 @@
 async function ping(ip) {
     let result = PingState.Failed;
     await $.get('https://steakovercooked.com/api/ping/?host=' + ip, function (data) {
-        if (data.includes('0% packet loss')) {
-            result =  PingState.Success;
+        if (data.includes(' 0% packet loss')) {
+            result = PingState.Success;
         }
     });
     return result;
